4f1991d41105cdf574a42b57700f6a80d0f71a47
elxrojo <elxrojo115@gmail.com>
2025-04-12 18:53:46 +0200

    refactor: cleaning conection

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,23 +1,20 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 
 // Connection URI
 const uri = process.env.ATLAS_URI;
 
 
-mongoose.connect(uri, {
-    useUnifiedTopology: true,
-});
+mongoose.connect(uri);
 
 const db = mongoose.connection;
 
 // Handler events for connection
 db.on('error', (error) => {
-    console.error('Error de conexión a MongoDB:', error);
+    console.error('Error connecting database:', error);
 });
 
 db.once('open', () => {
-    console.log('Conectado a MongoDB correctamente');
+    console.log('Database connected!');
 });
 
 // Export the connection and mongoose to use them in other files
