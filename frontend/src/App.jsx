927fe8672db6359fc121cd18a6d2dda9a4a21673
elxrojo <elxrojo115@gmail.com>
2025-04-13 15:25:48 +0200

    chore: add navbar base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className='h-full w-full bg-amber-200 flex justify-center'>
       <Navbar/>
+      
     </div>
   )
 }
--- frontend/src/components/Layout/Navbar/Navbar.jsx
+++ frontend/src/components/Layout/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
+import { FaCartShopping } from "react-icons/fa6";
+import { FaHistory } from "react-icons/fa";
+
+
 export default function Navbar(){
     return(
     <>
-        <div className="w-11/12 rounded-4xl m-5 p-4 justify-center border-solid border-2 border-b-cyan-700">
+        <div className="w-11/12 rounded-4xl m-5 py-4 px-10 flex justify-between  border-solid border-2 border-b-cyan-700">
             <div>
                 <h1>Pizzas Cloud</h1>
                 <h3>Tastes like a cloud</h3>
             </div>
-            <div></div>
+            <div className="h-full w-auto flex">
+                <button className="mx-3"><FaHistory  className="size-10"/></button>
+                <button className="mx-3"><FaCartShopping className="size-10"/></button>
+            </div>
         </div>
     </>
     )
