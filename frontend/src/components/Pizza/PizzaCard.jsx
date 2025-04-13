4ee4bffd7718a6a081b88324c8ba4be0de5a230e
elxrojo <elxrojo115@gmail.com>
2025-04-13 16:44:01 +0200

    chore: add pizza card

--- frontend/src/components/Pizza/PizzaCard.jsx
+++ frontend/src/components/Pizza/PizzaCard.jsx
@@ -1,12 +1,16 @@
-export default function PizzaCard () {
+export default function PizzaCard ({pizzaData}) {
     return(
-        <div className="w-2xs  rounded-3xl border-solid border-2 p-3 my-3 flex flex-col content-center items-center ">
-            <div className="bg-gray-400 w-2/3">Foto</div>
-            <h3>Name pizza</h3>
-            <p>Ingredientes:</p>
-            <li>Tomato</li>
-            <li>Tomato</li>
-            <li>Tomato</li>
+        <div className="w-2xs rounded-3xl border-solid border-2 p-3 my-3 flex flex-col content-center items-center ">
+            <img className="w-2/3" src={pizzaData.image}/>
+            <h3>{pizzaData.name}</h3>
+            <div className="text-left w-2/3 my-3">
+                <p>Ingredients:</p>
+                {
+                    pizzaData.ingredients.map((ingredient) => {
+                        return <li>{ingredient}</li>
+                    })
+                }
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,25 +1,36 @@
+import { useEffect, useState } from "react";
 import PizzaCard from "../../components/Pizza/PizzaCard";
+import { PizzaService } from "../../services/pizzaService";
+
 
 export default function Home () {
+    const [pizzas, setPizzas] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+        const fetchAllPizzas = async () => {
+            try{
+                const data = await PizzaService.getAll();
+                setPizzas(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchAllPizzas();
+    }, [])
+
     return(
         <div className="w-full bg-red-400">
             <h1>Here you can find the best pizzas!</h1>
             <div className="flex flex-wrap  justify-between">
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-                <PizzaCard/>
-
+                {
+                    pizzas.map((pizza) => {
+                        return <PizzaCard pizzaData={pizza}/>
+                    })
+                }
 
             </div>
         </div>
